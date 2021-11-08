5e8657cb9c0c48293049af9a615d717d08a02408
dudwns3020 <dudwns3020@naver.com>
2021-11-08 22:57:21 +0900

    Memeber, UsrMemberController, MemberService, MemberRepository 추가

---
+++ src/main/java/com/exam/demo/service/MemberService.java
@@ -0,0 +1,27 @@
+package com.exam.demo.service;
+
+import org.springframework.stereotype.Service;
+
+import com.exam.demo.repository.MemberRepository;
+import com.exam.demo.vo.Member;
+
+@Service
+public class MemberService {
+
+	private MemberRepository memberRepository;
+
+	public MemberService(MemberRepository memberRepository) {
+		this.memberRepository = memberRepository;
+	}
+
+	public int join(String loginId, String loginPw, String name, String nickname, String cellphoneNo, String email) {
+		memberRepository.join(loginId, loginPw, name, nickname, cellphoneNo, email);
+
+		return memberRepository.getLastInsertId();
+	}
+
+	public Member getMemberById(int id) {
+		return memberRepository.getMemberById(id);
+	}
+
+}
